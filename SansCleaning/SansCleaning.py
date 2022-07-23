c6888a938649feca1d28aa4ae45988db7288c3c4
La1xuan <bilibililaixuan@gmail.com>
2022-07-22 20:13:14 -0400

    updateForOrgBudgetFeature

--- SansCleaning/SansCleaning.py
+++ SansCleaning/SansCleaning.py
@@ -1,5 +1,6 @@
 from time import sleep
 from androidController import tap, tap2, matchImg, waitingFor, failSafe, timeoutSetting
+from json import loads, dumps
 def SansCleaning(status):
     status = int(status)
     cur = 0
@@ -40,6 +41,8 @@ def CheckSans(status,cur=0):
     #3: Use only res
     #4: Use org
     #Red state is needed
+    print("status:")
+    print(status)
     if cur > timeoutSetting:
         failSafe()
     if matchImg("Ready") != (0, 0):
@@ -47,33 +50,56 @@ def CheckSans(status,cur=0):
         tap2(matchImg("Ready"))
         return CheckSans(status,cur=cur+1)
     if matchImg("Set") != (0, 0):
+        print("Set found")
         tap2(matchImg("Set"))
         print("important Line executed")
         return 2
-    if matchImg("WithinTwoDays") != (0, 0):
+    if matchImg("WithinTwoDays",confidencevalue=0.99) != (0, 0):
+        print("WithinTwoDays found")
+        #sleep(10)
         if status > 0:
+            print("WithinTwoDays exec")
             tap(1357, 713)
             waitingFor("Ready")
             waitingFor("Set")
             return 1
-    if matchImg("WithinTheWeek") != (0, 0):
+    if matchImg("WithinTheWeek",confidencevalue=0.99) != (0, 0):
+        print("WithinTheWeek found")
+        #sleep(10)
         if status > 1:
+            print("WithinTheWeek exec")
             tap(1357, 713)
             waitingFor("Ready")
             waitingFor("Set")
             return 1
-    if matchImg("UseBoost") != (0, 0):
+    if matchImg("UseBoost",confidencevalue=0.99) != (0, 0):
+        print("UseBoost found")
+        #sleep(10)
         if status > 2:
+            print("UseBoost exec")
             tap(1357, 713)
             waitingFor("Ready")
             waitingFor("Set")
             return 1
-    if matchImg("BoostWithOrg") != (0, 0):
+    if matchImg("BoostWithOrg",confidencevalue=0.99) != (0, 0):
+        print("BoostWithOrg found")
+        print("Org budget: " + str(status - 3))
+        #sleep(10)
         if status > 3:
+            status -= 1
+            f = open("attributes.json", "r")
+            attributes = loads(f.read())
+            f.close()
+            attributes[2] = status
+            f = open("attributes.json", "w+")
+            f.write(dumps(attributes))
+            f.close()
+            
+            print("BoostWithOrg exec")
             tap(1357, 713)
             waitingFor("Ready")
             waitingFor("Set")
             return 1
-
+    print("not found")
     tap(978, 719)
     return 0
